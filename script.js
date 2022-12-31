14745ee79039efb75b9c2118445577cebf7bbcc1
Sabuj <sabujrouth2000@gmail.com>
2022-12-31 22:15:00 +0530

    add code

--- script.js
+++ script.js
@@ -8,5 +8,21 @@ const options = {
 
 fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=delhi', options)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then((response) => {
+
+
+        console.log(response)
+
+        cloud_pct = response.cloud_pct
+        temp = response.temp
+        feels_like = response.feels_like
+        humidity = response.humidity
+        min_temp = response.min_temp
+        max_temp = response.max_temp
+        wind_speed = response.wind_speed
+        wind_degrees = response.wind_degrees
+        sunrise = response.sunrise
+        sunset = response.sunset
+
+    })
     .catch(err => console.error(err));
