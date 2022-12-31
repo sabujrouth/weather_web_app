278368a7f07ac232c1e479ed2755edf8fcafd7ff
Sabuj <sabujrouth2000@gmail.com>
2022-12-31 22:52:29 +0530

    add code

--- script.js
+++ script.js
@@ -6,23 +6,35 @@ const options = {
     }
 };
 
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=delhi', options)
-    .then(response => response.json())
-    .then((response) => {
+const getWeather = (city) => {
+    cityName.innerHTML = city
+
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+        .then(response => response.json())
+        .then((response) => {
 
 
-        console.log(response)
+            console.log(response)
 
-        cloud_pct = response.cloud_pct
-        temp = response.temp
-        feels_like = response.feels_like
-        humidity = response.humidity
-        min_temp = response.min_temp
-        max_temp = response.max_temp
-        wind_speed = response.wind_speed
-        wind_degrees = response.wind_degrees
-        sunrise = response.sunrise
-        sunset = response.sunset
+            // cloud_pct.innerHTML = response.cloud_pct
+            temp.innerHTML = response.temp
+            feels_like.innerHTML = response.feels_like
+            humidity.innerHTML = response.humidity
+            min_temp.innerHTML = response.min_temp
+            max_temp.innerHTML = response.max_temp
+            wind_speed.innerHTML = response.wind_speed
+            wind_degrees.innerHTML = response.wind_degrees
+            sunrise.innerHTML = response.sunrise
+            sunset.innerHTML = response.sunset
 
-    })
-    .catch(err => console.error(err));
+        })
+        .catch(err => console.error(err));
+
+}
+
+submit.addEventListener("click", (e)=>{
+    e.preventDefault()
+    getWeather(city.value)
+})
+
+getWeather("Delhi")
